9ac2de0563d85f0792d109befb2861d938127241
Luca Fluri <luca.fluri4@gmail.com>
2020-03-21 23:10:15 +0100

    added reducer function according to slide

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,23 @@ import java.util.Arrays;
 
 
 public class Main {
-    private final int CHAINLENGTH = 2000;
-    private final int PASSLENGTH = 7;
+    private static final int CHAINLENGTH = 2000;
+    private static final int PASSLENGTH = 7;
     private static final char[] chars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q',
             'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
+    public static void main(String[] args){
 
-    /** Hashes String with MD5
-     *
+        System.out.println(hash("HELLO"));
+        System.out.println(reduce(hash("HELLO"), 0));
+
+
+    }
+
+
+    /**
+     * Hashes String with MD5
      * @param s String to hash in MD5
      * @return hashed String
      */
@@ -32,11 +40,28 @@ public class Main {
         return new BigInteger(1, bytes).toString(16);
     }
 
-    public static void main(String[] args){
 
-        System.out.println(hash("HELLO"));
-        System.out.println(hash("gg"));
+    /**
+     * Reduces the Hash according to Slide 3.27
+     * @param hash Hash value as String
+     * @param level current level
+     * @return reduced hash value
+     */
+    private static String reduce(String hash, int level){
+        BigInteger hashAsNum = new BigInteger(hash, 16);
+        hashAsNum = hashAsNum.add(BigInteger.valueOf(level));
+        StringBuilder sb = new StringBuilder();
+        BigInteger alphabetSize = BigInteger.valueOf(chars.length);
 
-
+        for(int i = 1; i<=PASSLENGTH; i++){
+            sb.append(chars[hashAsNum.mod(alphabetSize).intValue()]);
+            hashAsNum = hashAsNum.divide(alphabetSize);
+        }
+        sb.reverse();
+        return sb.toString();
     }
+
+
+
+
 }
