cf0b80ab70cbd62e3d09ea9c7994d83666cb0028
Luca Fluri <luca.fluri4@gmail.com>
2020-03-22 12:54:12 +0100

    removed unnecessary imports

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,3 @@
-import java.util.Map;
-
 public class Main {
     public static void main(String[] args){
         RainbowTable rt = new RainbowTable();
--- src/RainbowTable.java
+++ src/RainbowTable.java
@@ -1,9 +1,7 @@
-
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
